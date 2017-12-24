09f12f61d113c737c16ea8e65bc135e3d712b909
parker-hemphill <aaron.p.hemphill@gmail.com>
2017-12-23 21:34:54 -0500

    Update encode.sh

--- encode.sh
+++ encode.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# This script is adapted from the bash script located at 
-# https://gist.githubusercontent.com/donmelton/5734177/raw/79532cf60c90c40485cc4e546783fcb312ea4be3/encode.sh
-
 die() {
     echo "$program: $1" >&2
     exit ${2:-1}
@@ -23,22 +20,15 @@ fi
 handbrake="HandBrakeCLI"
 mediainfo="mediainfo"
 
-frame_rates_location="/path/to/Frame Rates"
-subtitles_location="/path/to/Subtitles"
-
-# My advice is: do NOT change these HandBrake options. I've encoded over 300
-# Blu-ray Discs, 30 DVDs and numerous other files with these settings and
-# they've never let me down.
-
-handbrake_options=" --markers --large-file --encoder x264 --encopts vbv-maxrate=25000:vbv-bufsize=31250:ratetol=inf --crop 0:0:0:0 --strict-anamorphic"
+handbrake_options=" --markers  -O --encoder x264 --encopts vbv-maxrate=25000:vbv-bufsize=31250:ratetol=inf --crop 0:0:0:0 "
 
 width="$(mediainfo --Inform='Video;%Width%' "$input")"
 height="$(mediainfo --Inform='Video;%Height%' "$input")"
 
 if (($width > 1280)) || (($height > 720)); then
-    max_bitrate="2500"
+    max_bitrate="4000"
 elif (($width > 720)) || (($height > 576)); then
-    max_bitrate="2000"
+    max_bitrate="3000"
 else
     max_bitrate="1800"
 fi
@@ -74,12 +64,7 @@ if [ ! "$frame_rate" ]; then
     frame_rate="$(mediainfo --Inform='Video;%FrameRate%' "$input")"
 fi
 
-frame_rate_file="$(basename "$input")"
-frame_rate_file="$frame_rates_location/${frame_rate_file%\.[^.]*}.txt"
-
-if [ -f "$frame_rate_file" ]; then
-    handbrake_options="$handbrake_options --rate $(cat "$frame_rate_file")"
-elif [ "$frame_rate" == '29.970' ]; then
+if [ "$frame_rate" == '29.970' ]; then
     handbrake_options="$handbrake_options --rate 23.976"
 else
     handbrake_options="$handbrake_options --rate 30 --pfr"
@@ -97,36 +82,8 @@ if [ "$frame_rate" == '29.970' ]; then
     handbrake_options="$handbrake_options --detelecine"
 fi
 
-srt_file="$(basename "$input")"
-srt_file="$subtitles_location/${srt_file%\.[^.]*}.srt"
-
-if [ -f "$srt_file" ]; then
-    subtitle_format="$(mediainfo --Inform='Text;%Format%' "$input" | sed q)"
-
-    if [ "$subtitle_format" == 'VobSub' ] || [ "$subtitle_format" == 'PGS' ]; then
-        handbrake_options="$handbrake_options --subtitle 1 --subtitle-burned"
-    else
-        tmp=""
-
-        trap '[ "$tmp" ] && rm -rf "$tmp"' 0
-        trap '[ "$tmp" ] && rm -rf "$tmp"; exit 1' SIGHUP SIGINT SIGQUIT SIGTERM
-
-        tmp="/tmp/${program}.$$"
-        mkdir -m 700 "$tmp" || exit 1
-
-        temporary_srt_file="$tmp/subtitle.srt"
-        cp "$srt_file" "$temporary_srt_file" || exit 1
-
-        handbrake_options="$handbrake_options --srt-file $(escape_string "$temporary_srt_file") --srt-codeset UTF-8 --srt-lang eng --srt-default 1"
-    fi
-fi
-
 output="${input%\.*}-converted.mp4"
 
-echo "Encoding: $input" >&2
-
-time "$handbrake" \
-    $handbrake_options \
-    --input "$input" \
-    --output "$output" \
-    2>&1 | tee -a "/tmp/converted"
+$handbrake $handbrake_options --input "$input" --output "$output" 2>&1 | tee -a "/tmp/converted_${2}"
+echo "No_conversion_active)" > /tmp/converted_${2}
+chmod 644 "$output"
